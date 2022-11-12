9d938534fa53edd8e8f45ae7fb7860da3a3102f8
Jan Kubny <30832483+SmartJSONEditor@users.noreply.github.com>
2022-11-12 11:04:10 -0600

    Updates

--- Sources/GoOutSafeAPIModels/LambdaResponse.swift
+++ Sources/GoOutSafeAPIModels/LambdaResponse.swift
@@ -11,10 +11,12 @@ public struct LambdaResponse: Codable {
     public let statuses: [String]?
     public let debugPrint: String?
     public let error: String?
+    public let tracker: PublicTracker?
     
-    public init(statuses: [String]? = nil, debugPrint: String? = nil, error: String? = nil) {
+    public init(statuses: [String]? = nil, debugPrint: String? = nil, error: String? = nil, tracker: PublicTracker? = nil) {
         self.statuses = statuses
         self.debugPrint = debugPrint
         self.error = error
+        self.tracker = tracker
     }
 }
